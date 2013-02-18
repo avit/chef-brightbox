d31ca9a361a78e0e1226736cc3582b434dd6d433
Andrew Vit <andrew@avit.ca>
2013-02-18 15:21:59 -0800

    Fine versions are available in non-experimental PPA

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -19,7 +19,7 @@
 
 apt_repository "brightbox-ruby-ng" do
   action       :add
-  uri          "http://ppa.launchpad.net/brightbox/ruby-ng-experimental/ubuntu"
+  uri          "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
   distribution node['lsb']['codename']
   components   ["main"]
   keyserver    "keyserver.ubuntu.com"
